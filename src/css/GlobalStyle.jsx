ee82ac8ac4f49b27bfde283e2df5125d1ff0761a
Yuji <gustavo_osugi@outlook.com>
2024-09-22 12:40:09 -0300

    Estilizando a ScrollBar

--- src/css/GlobalStyle.jsx
+++ src/css/GlobalStyle.jsx
@@ -12,12 +12,31 @@ const GlobalStyle = createGlobalStyle`
 
 html {
     scroll-behavior: smooth;
+    scrollbar-width: thin;
+    scrollbar-color: #1391D9 #161515; 
 }
 
 body {
   font-family: "Teko", sans-serif;
   font-optical-sizing: auto;
   font-style: normal;
+}
+
+::-webkit-scrollbar {
+    width: 12px; 
+}
+
+::-webkit-scrollbar-track {
+    background-color: #161515; 
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: #1391D9; 
+    border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background-color: #3F7085; 
 }
 
 `
